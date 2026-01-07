c54af5d4c3a9db827a3e095f2cb3da72f4d430bd
Geetha-Nagarajan <Geetha.Nagarajan@expleogroup.com>
2026-01-07 13:05:37 +0530

    restaurantMenuAdded

---
+++ restaurant_menu.js
@@ -0,0 +1,13 @@
+const breakfastMenu = ["Pancakes", "Eggs Benedict", "Oatmeal", "Frittata"];
+const mainCourseMenu = ["Steak", "Pasta", "Burger", "Salmon"];
+const breakfastMenuItemsHTML = breakfastMenu
+  .map((item, index) => `<p>Item ${index + 1}: ${item}</p>`)
+  .join("");
+document.getElementById("breakfastMenuItems").innerHTML =
+  breakfastMenuItemsHTML;
+
+let mainCourseItem = "";
+mainCourseMenu.forEach((item, index) => {
+  mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
+});
+document.getElementById("maincourseMenuItems").innerHTML = mainCourseItem;
